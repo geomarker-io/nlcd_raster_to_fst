cbb53e26bf95ef67d1a171436719f4661b8eac1b
Cole Brokamp <cole.brokamp@gmail.com>
2020-02-18 22:10:14 -0500

    add functions for extracting data

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # nlcd_raster_to_fst
 
-This repository contains an R script for downloading and transforming NLCD rasters into `.fst` files. In addition to the package dependencies documented in R, it relies on the `aws cli` being available too.
+## creating fst files
+
+This repository contains an R script for downloading and transforming NLCD rasters into `.fst` files. In addition to the package dependencies documented in `renv`, it relies on the `aws cli` being available too.
 
 This script relies heavily on system calls and so its behavior may change depending on the host operating system. Ideally, the `cole-brokamp/singr` singularity image would be used to run the script.
 
 - `nlcd_to_raster.R` downloads, extracts, and converts NLCD files to `.tif` files
+- NLCD files are stored at s3://geomarker/nlcd/nlcd_tif/ and include `impervious_{2001,2006,2011,2016}.tif`, `imperviousdescriptor_{2001,2006,2011,2016}.tif`, `nlcd_{2001,2006,2011,2016}.tif`, and `treecanopy{2011,2016}.tif`
 - `raster_to_fst.R` converts the `.tif` files into `.fst` files
+- NLCD data is stored as a folder of 1,685 `.fst` files at `s3://geomarker/nlcd/nlcd_fst/`
 
-Currently, NLCD files are stored at s3://geomarker/nlcd/nlcd_tif/ and include `impervious_{2001,2006,2011,2016}.tif`, `imperviousdescriptor_{2001,2006,2011,2016}.tif`, `nlcd_{2001,2006,2011,2016}.tif`, and `treecanopy{2011,2016}.tif`
+## reading data from fst files
+
+- each file is a "chunk" of the total data and is named like `nlcd_chunk_{chunk_number}.fst`, see `get_nlcd_data.R` for functions to extract data automatically based on NLCD cell number, year, and product name
+- chunk files will be automatically downloaded to the `.nlcd/` folder in the working directory; the number of chunk files needed depends on the geographic extent of the input spatial data; their size varies, but each file is 28.5 MB in size on average (all 1,685 files take about 48 GB on disk)
+
---
+++ get_nlcd_data.R
@@ -0,0 +1,57 @@
+library(dplyr)
+
+# choose random nlcd cell numbers for testing
+nlcd_cells <- sample(1:16832104560, 5)
+
+# predownloaded files exist for:
+nlcd_cells <- c(4511840538, 12586178917, 8228542109, 1808114857, 517112985)
+
+download_nlcd_chunk <- function(nlcd_chunk_number) {
+  dir.create("./nlcd_fst/", showWarnings = FALSE)
+  message(glue::glue("downloading s3://geomarker/nlcd/nlcd_chunk_{nlcd_chunk_number}.fst"))
+  download.file(
+    url = glue::glue(
+                  "https://geomarker.s3.us-east-2.amazonaws.com/",
+                  "nlcd/nlcd_fst/", "nlcd_chunk_{nlcd_chunk_number}.fst"
+                ),
+    destfile = glue::glue("./nlcd_fst/nlcd_chunk_{nlcd_chunk_number}.fst")
+  )
+}
+
+get_nlcd_data <- function(nlcd_cell_number,
+                          year = c(2001, 2006, 2011, 2016),
+                          product = c("nlcd", "impervious", "imperviousdescriptor")) {
+  if (length(nlcd_cell_number) > 1) {
+    warning("nlcd_cell is longer than one; processing only the first value")
+    nlcd_cell <- nlcd_cell_number[1]
+  }
+  nlcd_chunk <- nlcd_cell_number %/% 1e+07
+  nlcd_row <- nlcd_cell_number %% 1e+07
+  nlcd_file <- glue::glue("./nlcd_fst/nlcd_chunk_{nlcd_chunk}.fst")
+  nlcd_columns <- unlist(purrr::map(year, ~ glue::glue("{product}_{.}")))
+  if (!file.exists(nlcd_file)) download_nlcd_chunk(nlcd_chunk)
+  out <- fst::read_fst(
+                path = nlcd_file,
+                from = nlcd_row,
+                to = nlcd_row,
+                columns = nlcd_columns
+              )
+  out <- tibble::as_tibble(out)
+  out
+}
+
+get_nlcd_data(nlcd_cells[1])
+
+purrr::map_dfr(nlcd_cells, get_nlcd_data)
+
+purrr::map_dfr(nlcd_cells + 100000, get_nlcd_data)
+
+mappp::mappp(nlcd_cells, get_nlcd_data, parallel = TRUE)
+
+# download all chunks needed for nlcd multiple cell numbers ahead of time
+download_nlcd_chunks <- function(nlcd_cell_numbers) {
+  nlcd_chunks_needed <- unique(nlcd_cell_numbers %/% 1e+07)
+  purrr::walk(nlcd_chunks_needed, download_nlcd_chunk)
+}
+
+download_nlcd_chunks(nlcd_cells)
