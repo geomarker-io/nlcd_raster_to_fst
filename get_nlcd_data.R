8965ddfe46297b1b6a9abde31517ec2e1e060f3f
Cole Brokamp <cole.brokamp@gmail.com>
2020-03-03 14:37:10 -0500

    consider zero value of impervious type to be non-impervious

--- get_nlcd_data.R
+++ get_nlcd_data.R
@@ -58,47 +58,46 @@ r_nlcd_empty <-
     vals = NULL
   )
 
-# define legends for raster values
+# define legends for raster values (and our green codes)
 nlcd_legend <-
   tibble::tribble(
-    ~value, ~landcover_class,
-    11, "water",
-    12, "water",
-    21, "developed",
-    22, "developed",
-    23, "developed",
-    24, "developed",
-    31, "barren",
-    41, "forest",
-    42, "forest",
-    43, "forest",
-    51, "shrubland",
-    52, "shrubland",
-    71, "herbaceous",
-    72, "herbaceous",
-    73, "herbaceous",
-    74, "herbaceous",
-    81, "cultivated",
-    82, "cultivated",
-    90, "wetlands",
-    95, "wetlands"
+    ~value, ~landcover_class, ~landcover, ~green,
+    11, "water", "water", FALSE,
+    12, "water", "ice/snow", FALSE,
+    21, "developed", "developed open", TRUE,
+    22, "developed", "developed low intensity", TRUE,
+    23, "developed", "developed medium intensity", FALSE,
+    24, "developed", "developed high intensity", FALSE,
+    31, "barren", "rock/sand/clay", FALSE,
+    41, "forest", "deciduous forest", TRUE,
+    42, "forest", "evergreen forest", TRUE,
+    43, "forest", "mixed forest", TRUE,
+    51, "shrubland", "dwarf scrub", TRUE,
+    52, "shrubland", "shrub/scrub", TRUE,
+    71, "herbaceous", "grassland", TRUE,
+    72, "herbaceous", "sedge", TRUE,
+    73, "herbaceous", "lichens", TRUE,
+    74, "herbaceous", "moss", TRUE,
+    81, "cultivated", "pasture/hay", TRUE,
+    82, "cultivated", "cultivated crops", TRUE,
+    90, "wetlands", "woody wetlands", TRUE,
+    95, "wetlands", "emergent herbaceous wetlands", TRUE
   )
 
 imperviousness_legend <-
   tibble::tribble(
-            ~ value, ~ road_type, ~ urban_area,
-            1, "primary", TRUE,
-            2, "primary", FALSE,
-            3, "secondary", TRUE,
-            4, "secondary", FALSE,
-            5, "tertiary", TRUE,
-            6, "tertiary", FALSE,
-            7, "thinned", TRUE,
-            8, "thinned", FALSE,
-            9, "nonroad", TRUE,
-            10, "nonroad", FALSE,
-            11, "energy_prod", TRUE,
-            12, "energy_prod", FALSE
-          )
-
-
+    ~value, ~road_type,
+    0, "non-impervious",
+    1, "primary_urban",
+    2, "primary_nonurban",
+    3, "secondary_urban",
+    4, "secondary_nonurban",
+    5, "tertiary_urban",
+    6, "tertiary_nonurban",
+    7, "thinned_urban",
+    8, "thinned_nonurban",
+    9, "nonroad_urban",
+    10, "nonroad_nonurban",
+    11, "energy_prod_urban",
+    12, "energy_prod_nonurban",
+  )
